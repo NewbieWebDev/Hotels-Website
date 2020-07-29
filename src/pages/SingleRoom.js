0ef5552fc63943def91680860e3c58e3e9539986
NewbieWebDev <42526092+NewbieWebDev@users.noreply.github.com>
2020-07-29 16:00:54 +0800

    'starting-with-single-room-page'

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
+import defaultImg from '../images/room1.jpeg';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
+import {RoomContext} from '../context';
+
 
 export default class SingleRoom extends Component {
+    constructor(props){
+        super(props)
+
+        this.state={
+            slug:this.props.match.params.slug,
+            defaultImg
+        }
+    }
+
+    static contextType = RoomContext
+    
     render() {
+        const {getRoom} = this.context;
+        const room = getRoom(this.state.slug)
+
         return (
             <div>
                 hello from single room
+                
             </div>
         )
     }
